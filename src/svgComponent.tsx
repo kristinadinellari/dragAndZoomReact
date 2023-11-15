9ba8d80449d8e904d3237db7041a6d091fc69bed
Kristina Dinellari <kristinadinellari@gmail.com>
2023-11-15 15:53:51 +0100

    Add zoom in and zoom out buttons

--- src/svgComponent.tsx
+++ src/svgComponent.tsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import { SVG } from "./svg";
+import { SVG } from "./svg"; // if we will use this as a string
+import SVGTest from "./svgTest.svg"; // if we will use as an image
+// import { ReactComponent as SVGTest } from "./svgTest.svg"; if we will use this as a component directly
 
 interface SvgDragAndZoomState {
   isDragging: boolean;
@@ -48,11 +50,31 @@ class SvgDragAndZoom extends Component<{}, SvgDragAndZoomState> {
     }));
   };
 
-  zoom = (e: React.WheelEvent) => {
+  zoom = (e: any) => {
+    console.log(e, "event eee");
+    console.log(e.deltaY, "e.deltaY event");
     e.preventDefault();
 
     const scaleFactor = 1.2; // this value is to define how much you want to scale
-    const delta = e.deltaY > 0 ? scaleFactor : 1 / scaleFactor;
+    const delta = e.clientY > 0 ? scaleFactor : 1 / scaleFactor;
+
+    this.setState((prevState) => ({
+      viewBox: {
+        x: prevState.viewBox.x,
+        y: prevState.viewBox.y,
+        width: prevState.viewBox.width * delta,
+        height: prevState.viewBox.height * delta,
+      },
+    }));
+  };
+
+  zoom1 = (e: any) => {
+    console.log(e, "event eee");
+    console.log(e.deltaY, "e.deltaY event");
+    e.preventDefault();
+
+    const scaleFactor = 1.2; // this value is to define how much you want to scale
+    const delta = e.clientY < 0 ? scaleFactor : 1 / scaleFactor;
 
     this.setState((prevState) => ({
       viewBox: {
@@ -67,38 +89,44 @@ class SvgDragAndZoom extends Component<{}, SvgDragAndZoomState> {
   render() {
     const { viewBox } = this.state;
 
-    // const newViewBox = `${viewBox.x} ${viewBox.y} ${viewBox.width} ${viewBox.height}`;
+    const newViewBox = `${viewBox.x} ${viewBox.y} ${viewBox.width} ${viewBox.height}`;
 
-    // const parser = new DOMParser();
-    // const svgDocument = parser.parseFromString(SVG, "image/svg+xml");
+    const parser = new DOMParser();
+    const svgDocument = parser.parseFromString(SVG, "image/svg+xml");
 
-    // if (newViewBox) {
-    //   const svgElement = svgDocument.querySelector("svg");
-    //   if (svgElement) {
-    //     svgElement.setAttribute("viewBox", newViewBox);
-    //   }
-    // }
-    // const modifiedSvgString = new XMLSerializer().serializeToString(
-    //   svgDocument
-    // );
+    if (newViewBox) {
+      const svgElement = svgDocument.querySelector("svg");
+      if (svgElement) {
+        svgElement.setAttribute("viewBox", newViewBox);
+      }
+    }
+
+    const modifiedSvgString = new XMLSerializer().serializeToString(
+      svgDocument
+    );
 
     return (
       <div
         onMouseDown={this.startDrag}
         onMouseUp={this.endDrag}
         onMouseMove={this.drag}
-        onWheel={this.zoom}
-        style={{ width: "800px", height: "100hv", overflow: "hidden" }}
+        // onWheel={this.zoom}
+        style={{ width: "600px", height: "600px", overflow: "hidden" }}
       >
-        {/* <div
+        <button onClick={this.zoom}>Zoom out</button>
+        <button onClick={this.zoom1}>Zoom in</button>
+        <div
           dangerouslySetInnerHTML={{ __html: modifiedSvgString }}
           style={{ width: "100%", height: "100%" }}
-        /> */}
+        />
 
-        <svg
+        {/* <img src={SVGTest} alt="" /> */}
+        {/* <SVGTest /> */}
+
+        {/* <svg
           viewBox={`${viewBox.x} ${viewBox.y} ${viewBox.width} ${viewBox.height}`}
           dangerouslySetInnerHTML={{ __html: SVG }}
-        />
+        /> */}
       </div>
     );
   }
